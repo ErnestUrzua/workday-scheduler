0de73d5ac86cf1e72837332933136530d5986ddb
LoneStar <ernieurzua@gmail.com>
2020-06-24 20:57:49 -0700

    small updates to read me and script file

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     //set current time and Day
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-  
+    
+    //create event listeners for updates to hour cells and update them dynamically
+    // with set attribute
   
   });

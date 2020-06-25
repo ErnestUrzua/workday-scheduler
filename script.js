a499c5e7fbbaf7ff83dc9dd2a2c85591919b227e
ErnestUrzua <ernieurzua@gmail.com>
2020-06-24 23:00:16 -0700

    added text area blocks and changed some styling

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  //set current time and Day
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+  
+  //create event listeners for updates to hour cells and update them dynamically
+  // with set attribute
+  // $("#1").on("click", function () {
+
+  //   //  // if (($("#1").hasClass("textarea"))) {
+
+  //   //   }
+  //   //   else {
+  //   //     //select div
+  //   //    // var eventDiv = $("#1");
+  //   //     //append <textarea tags to div element
+  //   //    // eventDiv.append("<textarea></textarea>");
+  //   //   }
+
+  // });
+
+
+
+});

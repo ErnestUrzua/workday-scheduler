5e3ca08d941595decd03287f28995fde278a86d1
ErnestUrzua <ernieurzua@gmail.com>
2020-06-25 22:27:04 -0700

    cleaned up script.js, added styling tweaks to html, css

--- script.js
+++ script.js
@@ -11,47 +11,45 @@ $(document).ready(function () {
 
   //moment().hour(Number);//this sets the hr
   var currentHour = moment().hour() + 1; // this gets the hr +1 because function only does 0-23
-  console.log(currentHour);
+  console.log("Current hour is " + currentHour);
 
   //adds to the div class and changes it's color
   // $("#1").addClass("currentHour");
   // $("#1").css("visibility", "visible");
 
-  //highlight curent hour
+  //highlight curent hour by calling function
   function currentHr() {
-    $(".day").each(function() {
+    $(".day").each(function () {
 
-    var hour = parseInt($(this).attr("id"));
-    console.log(hour);
-    if (hour < currentHour){
-      $(this).addClass("pastHour");
-    }
+      var hour = parseInt($(this).attr("id"));
+      console.log(hour);
+      if (hour < currentHour) {
+        $(this).addClass("pastHour");
+      }
 
-    else if (hour === currentHour){
-      $(this).removeClass("pastHour");
-      $(this).addClass("currentHour");
-    }
-    else {
-      $(this).removeClass("pastHour");
-      $(this).removeClass("currentHour");
-    }
-    
+      else if (hour === currentHour) {
+        $(this).removeClass("pastHour");
+        $(this).addClass("currentHour");
+      }
+      else {
+        $(this).removeClass("pastHour");
+        $(this).removeClass("currentHour");
+      }
+
     })
-   
 
   }
 
   currentHr();
 
-
   //store <textarea> input in local storage
-  //when user presses save, 
-  $(".fa-save").click(function(){
+  //when user presses save
+  $(".fa-save").click(function () {
     // console.log("trying to save data");
     var id = $(this).parent().attr("id");
     var text = $(this).siblings(".event").val();// 
-    console.log(id,text);
-    
+    console.log(id, text);
+
     if (text !== null || text !== "") {
       localStorage.setItem(id, text);
     }
@@ -59,38 +57,12 @@ $(document).ready(function () {
 
   //be able to retrieve that data on refresh and populate the data
   //$("#9 .event").val(localStorage.getItem("9"));
-
-
-  $(".event").each(function() {
+  $(".event").each(function () {
 
     var tempId = $(this).parent().attr("id");//grabbing parent-div attr-id
-    
+
     $(this).val(localStorage.getItem(tempId));
 
+  })
 
-    // if (hour < currentHour){
-    //   $(this).addClass("pastHour");
-    // }
-    
-    })
-  
-
-  
-
-
-
-
-
-  //create event listeners for updates to hour cells and update them dynamically
-  // with set attribute
-  // $("#1").on("click", function () {
-
-  //   //  // if (($("#1").hasClass("textarea"))) {
-
-  //   //   }
-  //   //   else {
-  //   //     //select div
-  //   //    // var eventDiv = $("#1");
-  //   //     //append <textarea tags to div element
-  //   //    // eventDiv.append("<textarea></textarea>");
 });

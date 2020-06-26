95ef59eb54e88fa96bfac16d7c7ccb6b619dc741
ErnestUrzua <ernieurzua@gmail.com>
2020-06-25 23:28:21 -0700

    added comments

--- script.js
+++ script.js
@@ -17,20 +17,24 @@ $(document).ready(function () {
   // $("#1").addClass("currentHour");
   // $("#1").css("visibility", "visible");
 
-  //highlight curent hour by calling function
+  
   function currentHr() {
     $(".day").each(function () {
 
-      var hour = parseInt($(this).attr("id"));
+      var hour = parseInt($(this).attr("id")); //for each day class take id and put into hour
       console.log(hour);
-      if (hour < currentHour) {
+      //if the hour is less than the current hour we then give it the class pastHour
+      //this turns them gray
+      if (hour < currentHour) {  
         $(this).addClass("pastHour");
       }
 
+      // if hour is current then we highlight
       else if (hour === currentHour) {
         $(this).removeClass("pastHour");
         $(this).addClass("currentHour");
       }
+      //else we dont give it any classes and it will be default styling
       else {
         $(this).removeClass("pastHour");
         $(this).removeClass("currentHour");
@@ -40,18 +44,19 @@ $(document).ready(function () {
 
   }
 
-  currentHr();
+  currentHr(); //call function to check what the current hour is
 
   //store <textarea> input in local storage
   //when user presses save
   $(".fa-save").click(function () {
     // console.log("trying to save data");
-    var id = $(this).parent().attr("id");
-    var text = $(this).siblings(".event").val();// 
-    console.log(id, text);
+    var id = $(this).parent().attr("id"); //select the parent id for the save button
+    var text = $(this).siblings(".event").val();// take the text area of event class
+    console.log(id, text); 
 
+    //if the text is not null or empty 
     if (text !== null || text !== "") {
-      localStorage.setItem(id, text);
+      localStorage.setItem(id, text); //set the key and value into local storage
     }
   });
 
@@ -61,7 +66,7 @@ $(document).ready(function () {
 
     var tempId = $(this).parent().attr("id");//grabbing parent-div attr-id
 
-    $(this).val(localStorage.getItem(tempId));
+    $(this).val(localStorage.getItem(tempId));//get the item from storage using the Id 
 
   })
 
